87ee59ff8303538a6c2133ff949790e145ad4c08
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-02 14:50:37 +0900

    Update when.r

--- when.r
+++ when.r
@@ -1,11 +1,6 @@
-
 input <- readline("Enter value for K: ")
 K <- as.numeric(input)
-
 h <- 21 + floor(K / 60)
 m <- K %% 60
-
 ans <- sprintf("%02d:%02d", h, m)
-
 cat("Converted time:", ans, "\n")
-
