33cb38c1686f749a616ba8992dc841556ff2ec81
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-06 19:20:20 +0900

    Update abc.r

--- abc.r
+++ abc.r
@@ -2,11 +2,12 @@ input_line <- readLines("stdin", n = 1)
 input_values <- as.numeric(strsplit(input_line, " ")[[1]])
 
 N <- input_values[1]
-if (N <= 125)
+if (N <= 125) {
     result <- 4
-else if (N <= 211)
+} else if (N <= 211) {
     result <- 6
-else
+} else {
     result <- 8
+}
 
 cat(result, "\n")
