36daab76f05c2dee9eda66e7c541a8bf19c7a8f3
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-10 16:48:17 +0900

    Update input_print.r

--- input_print.r
+++ input_print.r
@@ -1,9 +1,28 @@
 
+################
+
 # input=3
 # input=a
 input <- readLines("stdin", n=1)
 s <- input[1]
 cat(s)
+
+################
+
+# input= 3 5
+input_line <- readLines("stdin", n = 1)
+input_values <- as.numeric(strsplit(input_line, " ")[[1]])
+a <- input_values[1]
+b <- input_values[2]
+cat(a, "\n")
+cat(b, "\n")
+
+################
+
+# input= 3 5
+input_line <- readLines("stdin", n = 1)
+input_values <- as.numeric(strsplit(input_line, " ")[[1]])
+cat(input_values, "\n")
 
 ################
 
