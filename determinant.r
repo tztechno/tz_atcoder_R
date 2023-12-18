3c1f222984221d8926ec27c802b324fdf42fa3f1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-18 12:06:38 +0900

    Update determinant.r

--- determinant.r
+++ determinant.r
@@ -1,3 +1,8 @@
+
+strsplit(input_line, " ") は、スペースを区切り文字として使用して、両方の入力行を部分文字列のリストに分割します。 
+二重角括弧 [[1]] と [[2]] は、結果のリストの最初と 2 番目の要素をそれぞれ抽出するために使用されます。 
+次に、各要素は as.numeric を使用して数値に変換されます。
+
 input_line <- readLines("stdin", n = 2)
 input_values1 <- as.numeric(strsplit(input_line, " ")[[1]])
 input_values2 <- as.numeric(strsplit(input_line, " ")[[2]])
@@ -11,6 +16,10 @@ ans <- a*d-b*c
 cat(ans, "\n")
 
 #######################################
+
+strsplit(input_line[1], " ") は、スペースを区切り文字として使用して、最初の入力行を部分文字列のベクトルに分割します。 
+二重角括弧 [[1]] は結果のリストの最初の要素を抽出するために使用され、as.numeric は部分文字列を数値に変換します。 
+2 番目の入力ラインにも同じプロセスが適用されます。
 
 input_line <- readLines("stdin", n = 2)
 input_values1 <- as.numeric(strsplit(input_line[1], " ")[[1]])
