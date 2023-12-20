64e4adba73bbaf342c2ad8a6283a53ceb08fa21e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-20 12:27:36 +0900

    Create follow.r

---
+++ follow.r
@@ -0,0 +1,9 @@
+//ABC182_A
+//Follow
+//input= 200 300
+input_line <- readLines("stdin", n = 1)
+input_values <- as.numeric(strsplit(input_line, " ")[[1]])
+A <- input_values[1]
+B <- input_values[2]
+MAXB=2*A+100
+cat(MAXB-B, "\n")
