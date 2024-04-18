cd77031e5a90ce4449e1902e608488732d131fd8
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-18 12:06:33 +0900

    Update abc073_b.r

--- abc073_b.r
+++ abc073_b.r
@@ -11,6 +11,20 @@ B <- as.numeric(strsplit(input_line[3], " ")[[1]])
 #######################################
 #######################################
 #######################################
+
+[WA2]
+
+input_lines <- readLines("stdin")
+N <- as.numeric(input_lines[1])
+t <- 0
+B <- numeric(N)  # Initialize B as a numeric vector of length N
+for (i in 1:N) {
+  A <- as.numeric(strsplit(input_lines[i+1], " ")[[1]])
+  B[i] <- A[2] - A[1] + 1  # Assign the difference to the ith element of B
+}
+t <- sum(B)
+cat(t, "\n")
+
 #######################################
 
 [WA2]
@@ -25,6 +39,8 @@ for (i in 1:N) {
 }
 
 cat(t, "\n")
+
+#######################################
 
 [WA2]
 
